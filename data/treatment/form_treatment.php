eb20b66541fc430fd44955342719806a0e76ff5b
Adidooouuu <79933094+Adidooouuu@users.noreply.github.com>
2022-06-07 16:23:58 +0200

    backend of the form (part of)

---
+++ data/treatment/form_treatment.php
@@ -0,0 +1,48 @@
+<?php
+  error_reporting(E_ALL);
+  ini_set("display_errors", 1);
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <?php require("../../templates/head.html"); ?>
+    <title>Recap</title>
+  </head>
+  <body>
+    <?php
+      function genererChaineAleatoire($longueur = 10)
+      {
+        $caracteres = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $longueurMax = strlen($caracteres);
+        $chaineAleatoire = '';
+        for ($i = 0; $i < $longueur; $i++)
+        {
+          $chaineAleatoire .= $caracteres[rand(0, $longueurMax - 1)];
+        }
+        return $chaineAleatoire;
+      }
+
+      if (isset( $_POST['submit']))
+      {
+        $text_content = htmlentities($_POST["text_content"]);
+        $link_name = htmlentities($_POST["name_of_paste_sharing"]);
+
+        $validation = "Your paste has been posted.";
+
+        $random_id = genererChaineAleatoire(5). + time();
+
+        $url = $_SERVER["PHP_SELF"]. "/" .$random_id;
+      }
+    ?>
+
+    <div class="container">
+      <div class="recap">
+        <span class="validation_message"><?php echo $validation; ?></span>
+        <h1 class="main_title">Recapitulation</h1>
+        <p class="shared_text_name"><?php echo "Name: " .$link_name; ?></p>
+        <p class="shared_text"><?php echo "Your text content: " .$text_content; ?></p>
+        <p class="sharing_url"><?php echo "Your sharing URL: " .$url ?></p>
+      </div>
+    </div>
+  </body>
+</html>
--- templates/form.php
+++ templates/form.php
@@ -1,16 +1,18 @@
 <div class="container">
-  <form class="main_form" action="../pages/form_treatment.php" method="post">
+  <form class="main_form" action="../data/treatment/form_treatment.php" method="post">
 
     <label for="text_content" class="label_textarea">New paste</label>
-    <textarea name="text_content"></textarea>
+    <textarea name="text_content">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </textarea>
 
     <fieldset class="fieldset_options">
       <legend>Optional paste settings</legend>
       <label for="name_of_paste_sharing" class="label_text">Paste name / title: </label>
-      <input type="text" name="name_of_paste_sharing">
+      <input type="text" name="name_of_paste_sharing" value="shared_text_name">
     </fieldset>
 
-    <input type="submit" value="Create new paste">
+    <input type="submit" value="Create new paste" name="submit">
   </form>
   <aside class="fantom"></aside>
 </div>
