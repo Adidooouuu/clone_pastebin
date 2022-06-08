cc73e393c5081a0cb2e70ee7f454835c4cad676f
Adidooouuu <79933094+Adidooouuu@users.noreply.github.com>
2022-06-08 16:53:29 +0200

    broke

--- data/connection_to_db/db_connection.php
+++ data/connection_to_db/db_connection.php
@@ -1,4 +1,5 @@
 <?php
+  include('../../config.php');
   // DEFINED DATAS
   $hostname = constant("HOST");
   $username = constant('USER');
@@ -7,11 +8,33 @@
   $table = constant('LINK_TABLE');
 
   // DB CONNECTION
-  $connect = mysqli_connect($hostname, $username, $pwd);
-  mysqli_select_db($connect, $bdd);
-
-  if(!$connect)
+  try
   {
-    die('Could not Connect MySql Server:' .mysql_error());
+    $bdd_connection = new PDO(
+                                "mysql:host=$hostname; dbname=$bdd",
+                                $username,
+                                $pwd,
+                                [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION],
+                              );
+  }
+  catch(Exception $e)
+  {
+    die('Erreur : ' . $e->getMessage());
+  }
+
+  // PULL DB DATA
+  $table_query = "SELECT * FROM $table";
+  $table_statement = $bdd_connection->prepare($table_query);
+  $table_statement->execute();
+  $table_fetch = $table_statement->fetchAll();
+
+  foreach ($table_fetch as $table_content)
+  {
+    ?>
+      <p><?php echo $table_content['title']; ?></p>
+      <p><?php echo $table_content['content']; ?></p>
+      <p><?php echo $table_content['random_id']; ?></p>
+      <p><?php echo $table_content['creation_date']; ?></p>
+    <?php
   }
 ?>
--- data/treatment/form_treatment.php
+++ data/treatment/form_treatment.php
@@ -1,70 +1,38 @@
 <?php
-  error_reporting(E_ALL);
-  ini_set("display_errors", 1);
-  mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-
-  include_once('../../config.php');
   include('../connection_to_db/db_connection.php');
+
+  function genererChaineAleatoire($longueur = 10)
+  {
+    $caracteres = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $longueurMax = strlen($caracteres);
+    $chaineAleatoire = '';
+    for ($i = 0; $i < $longueur; $i++)
+    {
+      $chaineAleatoire .= $caracteres[rand(0, $longueurMax - 1)];
+    }
+    return $chaineAleatoire;
+  }
+
+  if (isset( $_POST['submit']))
+  {
+    // PULL FORM DATA
+    $link_name = htmlentities($_POST["name_of_paste_sharing"]);
+    $text_content = htmlentities($_POST["text_content"]);
+
+    // URL WITH RANDOM ID
+    $random_id = genererChaineAleatoire(5). + time();
+    // $url = constant("WEBSITE") .$_SERVER["PHP_SELF"]. "+" .$random_id;
+
+    // AD DATA TO DB
+    $table_add_query = "INSERT INTO shared_link (title, content, random_id) VALUES (:link_name, :text_content, :random_id)";
+    $insert_new_content = $bdd_connection->prepare($table_add_query);
+    $insert_new_content->execute(
+                                  [
+                                    "link_name" => $link_name,
+                                    "text_content" => $text_content,
+                                    "random_id" => $random_id,
+                                  ]
+                                );
+    header("Location: ../../templates/form_treated.php");
+  }
 ?>
-<!DOCTYPE html>
-<html lang="fr" dir="ltr">
-  <head>
-    <?php require("../../templates/head.php"); ?>
-    <title>Recap</title>
-  </head>
-  <body>
-    <?php
-      function genererChaineAleatoire($longueur = 10)
-      {
-        $caracteres = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-        $longueurMax = strlen($caracteres);
-        $chaineAleatoire = '';
-        for ($i = 0; $i < $longueur; $i++)
-        {
-          $chaineAleatoire .= $caracteres[rand(0, $longueurMax - 1)];
-        }
-        return $chaineAleatoire;
-      }
-
-      if (isset( $_POST['submit']))
-      {
-        // PULL FORM DATA
-        $link_name = htmlentities($_POST["name_of_paste_sharing"]);
-        $text_content = htmlentities($_POST["text_content"]);
-
-        // URL WITH RANDOM ID
-        $random_id = genererChaineAleatoire(5). + time();
-        $url = constant("WEBSITE") .$_SERVER["PHP_SELF"]. "/" .$random_id;
-
-        $sql = "INSERT INTO shared_link (title, content, random_id) VALUES ('$link_name','$text_content','$random_id')";
-
-        if (mysqli_query($connect, $sql))
-        {
-          echo "Your paste is available.";
-        }
-        else
-        {
-          echo "Error: " . $sql . ":-" . mysqli_error($connect);
-        }
-        mysqli_close($connect);
-      }
-        // PULL DB DATA
-        $query = "SELECT * FROM $table";
-        $result = mysqli_query($connect, $query);
-        $row = mysqli_fetch_all($result);
-
-        echo "<pre>";
-        print_r($row);
-        echo "</pre>";
-    ?>
-
-    <div class="container">
-      <div class="recap">
-        <h1 class="main_title">Recapitulation</h1>
-        <p class="shared_text_name"><?php echo "Name: " .$link_name; ?></p>
-        <p class="shared_text"><?php echo "Your text content: " .$text_content; ?></p>
-        <p class="shared_url"><?php echo "Your URL: " .$url; ?></p>
-      </div>
-    </div>
-  </body>
-</html>
